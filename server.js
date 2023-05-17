3f67ba635a344a21c73a692998922dad89833829
Harrison Dutson <totodutson@gmail.com>
2023-05-17 12:25:11 -0600

    serves front end with express.static instead of indivisual endpoints

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const app = express()
 
 app.use(express.json())
 
-console.log()
+app.use(express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/public/index.html")
+// })
 
-app.get('/styles', (req, res) => {
-    res.sendFile(__dirname + "/public/styles.css")
-})
+// app.get('/styles', (req, res) => {
+//     res.sendFile(__dirname + "/public/styles.css")
+// })
 
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + "/public/main.js")
-})
+// app.get('/js', (req, res) => {
+//     res.sendFile(__dirname + "/public/main.js")
+// })
 
 app.listen(4000, () => {
     console.log("up on 4000")
